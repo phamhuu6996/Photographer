25cfa44240e36e184d5410278820609f92220dff
phamhuu6996 <phamhuu6996@gmail.com>
2026-02-06 23:19:05 +0700

    feat: update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.phamhuu.photographer"
         minSdk = 28
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.1.5"
+        versionCode = 9
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
